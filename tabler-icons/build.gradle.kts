70b1745e8a557d8b0c031e6e316d057a5a13737c
Benjamin <benjaminhalko@hotmail.com>
2024-01-19 16:29:35 -0800

    Updated

--- tabler-icons/build.gradle.kts
+++ tabler-icons/build.gradle.kts
@@ -1,12 +1,32 @@
 plugins {
-    kotlin("multiplatform")
+    //kotlin("multiplatform")
     id("com.android.library")
     id("org.jetbrains.compose")
     id("com.vanniktech.maven.publish")
 }
 
-setupModuleForComposeMultiplatform()
+//setupModuleForComposeMultiplatform()
 
 android {
     namespace = "compose.icons.tablericons"
+}
+
+android {
+    compileSdkVersion(33)
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        //versionCode = 1
+        //versionName = "1.0"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "local"
+            url = uri("file://${System.getenv("DEPLOY_DIR")}")
+        }
+    }
 }
